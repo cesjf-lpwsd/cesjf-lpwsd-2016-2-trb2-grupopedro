748ac3851340a8ee48d47a86187e3ca4da0f4fa9
Igorfernandes31 <igorsfernandes31@gmail.com>
2016-12-18 11:46:19 -0200

    adiciona jsf

---
+++ src/java/br/cesjf/lpwsd/trab2/academia/AlunoFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cesjf.lpwsd.trab2.academia;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Dara
+ */
+@Stateless
+public class AlunoFacade extends AbstractFacade<Aluno> {
+
+    @PersistenceContext(unitName = "Trab2PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AlunoFacade() {
+        super(Aluno.class);
+    }
+    
+}
